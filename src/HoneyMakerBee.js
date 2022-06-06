45bc9cc506516998c508c015fbbccae1c5e36dc2
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-06 11:14:53 -0400

    pass all tests for beesbeesbees

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,8 +1,13 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
   this.age = 10;
   this.job = 'make honey';
   this.honeyPot = 0;
+  this.color = 'yellow';
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
 
 HoneyMakerBee.prototype.makeHoney = function() {
   this.honeyPot++;
